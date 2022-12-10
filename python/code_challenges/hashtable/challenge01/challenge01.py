cc134d2948630e41dcd658a22af9f6c643c314bc
amani51 <amani.898919@gmail.com>
2022-12-10 16:45:03 +0200

    docs: add docs string

--- python/code_challenges/hashtable/challenge01/challenge01.py
+++ python/code_challenges/hashtable/challenge01/challenge01.py
@@ -2,6 +2,10 @@
 from Classes import * 
 
 def two_sum_bst(root, k):
+    '''
+    A function that takes a Binary Search tree and an integer as a parameter. 
+    Return True if Binary search tree has two elements that their summation is the given integer
+    '''    
     def inner(root):
         if not root:
             return False
